95f149bbede0821ebdee3f5bfd15ea0baa9ebe24
SalomeLefebvre <76622700+SalomeLefebvre@users.noreply.github.com>
2024-02-29 13:59:31 +0100

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,8 @@ def add(x, y):
   return x + y
 # test_sum.py
 from sum import add
+
 def test_add():
-  assert add(2, 3) == 5
-  assert add(-1, 1) == 0
-  assert add(-1, -1) == -2
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(-1, -1) == -2
